39c050ff238300409feca87f4c9ce3c6149bf7b1
kvkcon <1820270859@qq.com>
2025-05-24 17:04:28 +0800

    fix: update pip-setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages
 from distutils.core import setup
 
-setup(name='unitree_rl_gym',
+setup(name='rl_g1_punch',
       version='1.0.0',
-      author='Unitree Robotics',
+      author='kvkcon',
       license="BSD-3-Clause",
       packages=find_packages(),
-      author_email='support@unitree.com',
-      description='Template RL environments for Unitree Robots',
+      author_email='',
+      description='punch walk for Unitree Robots',
       install_requires=['isaacgym', 'rsl-rl', 'matplotlib', 'numpy==1.20', 'tensorboard', 'mujoco==3.2.3', 'pyyaml'])
